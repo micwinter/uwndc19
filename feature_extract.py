2d375d2e6c832d1d590cd1ebe44946708bac616b
michele <michele@fentanyl.jlg-lab>
2019-01-15 13:34:17 -0800

    Starting UWNDC19 challenge

---
+++ feature_extract.py
@@ -0,0 +1,14 @@
+"""
+Extract features from data using conv net for use in linear regression
+"""
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+import os
+
+
