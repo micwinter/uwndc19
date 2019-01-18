2efae3e2bcb5d231d74c358fad7b768e82acd5ed
Michele Winter <minter@Micheles-MacBook-Pro.local>
2019-01-18 10:01:17 -0800

    Trying to setup NNModel from AlexNet

--- nn_model.py
+++ nn_model.py
@@ -17,11 +17,28 @@ from torch.optim import lr_scheduler
 import copy
 import time
 
+def rmse(x, y):
+    return np.sqrt(np.mean((x-y) ** 2))
 
 class NNModel:
 
     def __init__(self):
-        self = self
-        model = models.alexnet(pretrained = True)
-        self.features = self.model.features[0] # first conv2d layer
+        self = models.alexnet(pretrained = True)
+        self.features = self.features[0] # first conv2d layer
         self.classifier = nn.Linear(64 * 55 * 55, 18) # 18 neurons
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        return x
+
+    def fit():
+        break
+
+    def predict():
+        break
+
+def nnmodel():
+    model = NNModel()
+    return model
